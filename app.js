b2bba4bd1fed7f796a43f52d6fdf64a36308fe96
GAdamAkos <146832623+GAdamAkos@users.noreply.github.com>
2025-07-21 09:37:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 const port = process.env.PORT || 3000;
 
 const dbPath = path.resolve(__dirname, 'whatsapp_messages.db');
